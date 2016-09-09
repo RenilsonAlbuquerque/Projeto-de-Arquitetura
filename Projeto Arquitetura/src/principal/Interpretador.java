a50cff336fe34f3c27a8d0bd95ac1b0344732c6f
Mateus <a.mateuslol@gmail.com>
2016-09-09 16:47:29 -0300

    Alterações das funções de tipo R

--- Projeto Arquitetura/src/principal/Interpretador.java
+++ Projeto Arquitetura/src/principal/Interpretador.java
@@ -1,7 +1,5 @@
 package principal;
 
-import java.util.ArrayList;
-
 public class Interpretador {
 
 	public Interpretador() {
@@ -36,7 +34,7 @@ public class Interpretador {
 	
 	
 
-	public String translateAssembly(String result) {
+	public void translateAssembly(String result) {
 
 		// ---------- separação do opCode ---------//
 
@@ -46,7 +44,7 @@ public class Interpretador {
 
 		// -------- estruturação do tipo r -------//
 
-		String rs, rt, rd, sh, fn;
+		String rs, rt, rd, sh, fn,imm,L;
 
 		// ---------------------------------------//
 
@@ -80,99 +78,118 @@ public class Interpretador {
 
 			case "100000":
 				// ADD
-				
-				
+				System.out.println("Add " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 
 				break;
 
 			case "100001":
 				// ADDU
+				System.out.println("Addu " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 
 				break;
 
 			case "100100":
 				// AND
-
+				System.out.println("And " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "001000":
 				// JR
-
+				System.out.println("Jr " +"$"+toDecimal(rs));
 				break;
 
 			case "010000":
 				// MFHI
+				System.out.println("Mfhi " +"$"+toDecimal(rd));
 				break;
 
 			case "010010":
 				// MFLO
+				System.out.println("Mflo " +"$"+ toDecimal(rd) );
 				break;
 
 			case "011000":
 				// MULT
+				System.out.println("Mult " +"$"+toDecimal(rs)+ "$" +toDecimal(rt));
 				break;
 
 			case "011001":
 				// MULTU
+				System.out.println("Multu " +"$"+toDecimal(rs)+ " $"+ toDecimal(rt) );
 				break;
 
 			case "100111":
 				// NOR
+				System.out.println("Nor " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "100101":
 				// OR
+				System.out.println("Or " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "000000":
 				// SLL
+				System.out.println("Sll " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(sh) );
 				break;
 
 			case "000100":
 				// SLLV
+				System.out.println("Sllv " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(rs) );
 				break;
 
 			case "101010":
 				// SLT
+				System.out.println("Slt " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "000011":
 				// SRA
+				System.out.println("Sra " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(sh) );
 				break;
 
 			case "000111":
 				// SRAV
+				System.out.println("Srav " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(rs) );
 				break;
 
 			case "000010":
 				// SRL
+				System.out.println("Srl " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(sh) );
 				break;
 			case "000110":
 				// SRLV
+				System.out.println("Srlv " +"$"+toDecimal(rd)+ "$" +toDecimal(rt)+"$"+ toDecimal(rs) );
 				break;
 
 			case "100010":
 				// SUB
+				System.out.println("Sub " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "100011":
 				// SUBU
+				System.out.println("Sub " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "001100":
 				// SYSCALL
+				System.out.println("Syscall");
 				break;
 
 			case "100110":
 				// XOR
+				System.out.println("Xor " +"$"+toDecimal(rd)+ "$" +toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "011011":
 				// DIVU
+				System.out.println("Divu "+ "$"+toDecimal(rs)+"$"+ toDecimal(rt) );
 				break;
 
 			case "011010":
 				// DIV
+				System.out.println("Div " +"$"+toDecimal(rs)+ "$" +toDecimal(rt) );
 				break;
 
 			}
@@ -180,13 +197,11 @@ public class Interpretador {
 			break;
 
 		case "000001":
-
 			// BLTZ
-
+			
 			break;
 
 		case "000010":
-
 			// J
 
 			break;
@@ -283,7 +298,6 @@ public class Interpretador {
 
 		}
 
-		return null;
 
 	}
 
